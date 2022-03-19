d9e864a72e476eac646ce4b6a01992ec62628851
fariarahman15 <faria.khanam@northsouth.edu>
2022-03-20 05:15:37 +0600

    version 1

--- JS/money_manager.js
+++ JS/money_manager.js
@@ -1,3 +1,4 @@
+/* functions */
 // get input amount 
 function getInputAmount(inputID){
     const getInputAmount = document.getElementById(inputID);
@@ -18,15 +19,29 @@ function getTotalExpenses(){
 };
 
 
-// calculate & update balance 
+// calculate balance 
 function getBalance(minuendAmount, subtrahendAmount){
     const availableBalance = minuendAmount - subtrahendAmount;
-    const totalBalance = document.getElementById('total-balance');
-    totalBalance.innerText = availableBalance;
     return availableBalance;
 };
 
-document.getElementById('calculation').addEventListener('click',function(){
+
+// calculate & update savings
+function getsavings(currentBalance){
+    const savingsPercentage = getInputAmount('Percentage');
+    const savingAmonut = (savingsPercentage/100) * currentBalance;
+    const totalsavings = document.getElementById('saving-amount');
+    totalsavings.innerText = savingAmonut;
+    return savingAmonut;
+}
+
+
+/* ---------------------------------- */
+/* click event */ 
+/* ---------------------------------- */
+
+// expenses & balance update 
+document.getElementById('calculate-button').addEventListener('click',function(){
     // get income amount 
     const incomeAmount = getInputAmount('income-amount');
 
@@ -34,6 +49,24 @@ document.getElementById('calculation').addEventListener('click',function(){
     const expensesAmount = getTotalExpenses();
 
     // update balance 
-    const currentBalance = getBalance(incomeAmount, expensesAmount);
+    const currentBalance = getBalance(incomeAmount, expensesAmount); 
+    const totalBalance = document.getElementById('total-balance');
+    totalBalance.innerText = currentBalance;
+});
 
+
+// saving amount & remaining balance update 
+document.getElementById('save-button').addEventListener('click',function(){
+    const incomeAmount = getInputAmount('income-amount'); 
+    const expensesAmount = getTotalExpenses();
+    const currentBalance = getBalance(incomeAmount, expensesAmount); 
+
+    // update saving amount 
+    const savings = getsavings(currentBalance);
+    
+    // update remaining balance 
+    const remainingBalance = getBalance(currentBalance, savings);
+    const updatedlBalance = document.getElementById('remaining-balance');
+    updatedlBalance.innerText = remainingBalance;
+    
 });
