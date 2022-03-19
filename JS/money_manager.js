9aa25ba3b24cd7d0f0295044e4cd0c26f12e11be
fariarahman15 <faria.khanam@northsouth.edu>
2022-03-20 04:09:37 +0600

    calculation part

--- JS/money_manager.js
+++ JS/money_manager.js
@@ -1,16 +1,39 @@
-/* calculation */
-function getInputAmount(){
-    const getIncomeAmount = document.getElementById('income-amount');
-    const incomeAmountText = getIncomeAmount.value;
-    const incomeAmount = parseFloat(incomeAmountText);
-    console.log(incomeAmount);
-}
+// get input amount 
+function getInputAmount(inputID){
+    const getInputAmount = document.getElementById(inputID);
+    const inputAmountText = getInputAmount.value;
+    const inputAmount = parseFloat(inputAmountText);
+    return inputAmount;
+};
 
+// get & update total expenses 
+function getTotalExpenses(){
+    const foodExpenses = getInputAmount('food-expenses');
+    const rentExpenses = getInputAmount('rent-expenses');
+    const clothExpenses = getInputAmount('cloth-expenses');
+    const totalExpenses = foodExpenses + rentExpenses + clothExpenses;
+    const totalExpensesAmount = document.getElementById('total-expenses');
+    totalExpensesAmount.innerText = totalExpenses;
+    return totalExpenses;
+};
+
+
+// calculate & update balance 
+function getBalance(minuendAmount, subtrahendAmount){
+    const availableBalance = minuendAmount - subtrahendAmount;
+    const totalBalance = document.getElementById('total-balance');
+    totalBalance.innerText = availableBalance;
+    return availableBalance;
+};
 
 document.getElementById('calculation').addEventListener('click',function(){
-    /* const getIncomeAmount = document.getElementById('income-amount');
-    const incomeAmountText = getIncomeAmount.value;
-    const incomeAmount = parseFloat(incomeAmountText);
-    console.log(incomeAmount); */
-    getInputAmount();
+    // get income amount 
+    const incomeAmount = getInputAmount('income-amount');
+
+    // update expenses amount 
+    const expensesAmount = getTotalExpenses();
+
+    // update balance 
+    const currentBalance = getBalance(incomeAmount, expensesAmount);
+
 });
